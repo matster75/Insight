8cb5fac28e5d56b587645cf03c16c538f6d9c04b
matster75 <38755750+matster75@users.noreply.github.com>
2020-10-02 10:30:16 +0000

    Deploying to gh-pages from master @ d3ad96024ba8f8f502f6a05f0265db9ed1ba1b03 🚀

---
+++ INSIGHT LOGO WITH STRAP V2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="INSIGHT LOGO WITH STRAP V2" tilewidth="32" tileheight="32" tilecount="696" columns="58">
+ <image source="../../../Business/Dropbox (Insight)/3.Project Managers Folders/Logos/Insight/New Insight logo/INSIGHT LOGO WITH STRAP V2.jpg" width="1870" height="407"/>
+</tileset>
