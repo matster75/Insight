1bf6f670763af85b2bfc03f41c4d26d446e14a22
matster75 <38755750+matster75@users.noreply.github.com>
2020-10-16 10:45:32 +0100

    ss

--- INSIGHT LOGO WITH STRAP V2.tsx
+++ INSIGHT LOGO WITH STRAP V2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="INSIGHT LOGO WITH STRAP V2" tilewidth="33" tileheight="32" tilecount="696" columns="17">
+<tileset version="1.4" tiledversion="1.4.2" name="INSIGHT_Logo" tilewidth="33" tileheight="32" tilecount="51" columns="17">
  <grid orientation="orthogonal" width="32" height="32"/>
- <image source="30 - INSIGHT LOGO WITH STRAP V2.jpg" width="561" height="123"/>
+ <image source="30_ INSIGHT_LOGO.jpg" width="561" height="123"/>
 </tileset>
--- REVO - V&E.tsx
+++ REVO - V&E.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="20 - REVO - V&amp;E" tilewidth="34" tileheight="34" margin="1" tilecount="45" columns="9">
+<tileset version="1.4" tiledversion="1.4.2" name="V_and_E" tilewidth="34" tileheight="34" margin="1" tilecount="45" columns="9">
  <grid orientation="orthogonal" width="35" height="35"/>
- <image source="40 - REVO - V&amp;E.jpg" width="312" height="196"/>
+ <image source="40-V_E.jpg" width="312" height="196"/>
 </tileset>
