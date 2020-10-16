5864d7a1764d46a9128e2e8b7faafdd1c2a2989a
matster75 <38755750+matster75@users.noreply.github.com>
2020-10-16 10:19:29 +0000

    Deploying to gh-pages from master @ fbd0162b09fe3a4176ebd51c6a5408d12ee5492a 🚀

--- INSIGHT LOGO WITH STRAP V2.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="INSIGHT_Logo" tilewidth="33" tileheight="32" tilecount="51" columns="17">
- <grid orientation="orthogonal" width="32" height="32"/>
- <image source="30_ INSIGHT_LOGO.jpg" width="561" height="123"/>
-</tileset>
